5e65421ceadc5f379a13a64e22b1a57a33c12f31
Tejas <tejastarghale@gmail.com>
2024-04-30 12:00:23 +0530

    Tejas Branch Python3 code

---
+++ Python3_Pipeline.py
@@ -0,0 +1,8 @@
+def calculate_rectangle_area():
+    length = 5  # Length of the rectangle
+    width = 3  # Width of the rectangle
+    return length * width
+
+
+area = calculate_rectangle_area()
+print("The area of the rectangle is:", area)
